39b95b8f2a92cdd11436c251c94312c568612487
emir <emirariomatic@gmail.com>
2024-05-01 21:53:48 +0300

    Watch connectivity complete

---
+++ ios/CoinzPricezWatchApp Watch App/CoinzPricezWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinzPricezWatchAppApp.swift
+//  CoinzPricezWatchApp Watch App
+//
+//  Created by Emir Arı on 1.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinzPricezWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/CoinzPricezWatchApp Watch App/PhoneConnector.swift
@@ -0,0 +1,8 @@
+//
+//  PhoneConnector.swift
+//  CoinzPricezWatchApp Watch App
+//
+//  Created by Emir Arı on 1.05.2024.
+//
+
+import Foundation
